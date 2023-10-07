feacc155e5ee370b0d9d9592df9b1258cfcf3245
Kostiantyn Yatsenko <moahtdeep@gmail.com>
2023-10-07 22:29:00 +0300

    Set up next-auth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,13 @@
+'use client';
 import React from 'react'
 import Link from 'next/link'
+import { useSession } from 'next-auth/react'
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
-    <div className='flex justify-end my-5 bg-slate-200'>
+    <div className='flex justify-end my-5 bg-slate-200 space-x-3'>
       <nav className='p-5'>
         <Link href='/'>Home</Link>
       </nav>
@@ -13,6 +17,18 @@ const NavBar = () => {
       <nav className='p-5'>
         <Link href='/products'>Products</Link>
       </nav>
+
+      {status === 'loading' && <div>Loading...</div>}
+      {
+        status === 'authenticated' &&
+        <div>{session.user!.name}</div>
+      }
+      {
+        status === 'unauthenticated' &&
+        <nav className='p-5'>
+          <Link href='/api/auth/signin'>Sign in</Link>
+        </nav>
+      }
     </div>
   )
 }
